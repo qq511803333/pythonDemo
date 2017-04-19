38c3e39ee3fa94e38e81b7529c430c914b30332b
qq511803333 <511803333@qq.com>
2017-04-19 13:22:10 +0800

    重新整理

--- 12306/zuowei.py
+++
@@ -1,9 +0,0 @@
-msg = []
-a = {"a":6}
-b = {"b":6}
-c = {"x":6}
-
-
-msg.append(b)
-msg.append(c)
-print(msg[0].keys())
--- baike/config.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-
-DBHOST = 'localhost'
-DBPORT = 3306
-DBUSER = "root"
-DBPASSWORD = "guang888"
-DBNAME = "mydatabase"
-DBCHAR = "utf8"
